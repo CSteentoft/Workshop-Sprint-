160b969f748a224b4623c5d88a1d6163036d31a9
CSteentoft <chste23@student.sdu.dk>
2023-11-22 13:44:49 +0100

    Actuator class methods and attributes

--- src/main/java/org/example/Actuator.java
+++ src/main/java/org/example/Actuator.java
@@ -1,4 +1,15 @@
 package org.example;
 
 public class Actuator {
+    Double value;
+    String name;
+    int actuatorID;
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public Double getValue() {
+        return value;
+    }
 }
